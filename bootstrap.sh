8d9194d0cd84db85a609ef37d8b1e42f9bf8f433
Sebastien Thomas <sebastien.thomas@livingsocial.com>
2015-07-04 02:32:00 -0400

    Change box to centos 7

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,51 +1,55 @@
 # The output of all these installation steps is noisy. With this utility
 # the progress report is nice and concise.
+
+#https://github.com/rails/rails-dev-box
 function install {
     echo installing $1
     shift
-    apt-get -y install "$@" >/dev/null 2>&1
+	yum -y install "$@"
+}
+
+function uninstall {
+    echo uninstalling $1
+    shift
+	yum -y erase "$@"
 }
 
 echo updating package information
-apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
-apt-get -y update >/dev/null 2>&1
+#yum -y update >/dev/null 2>&1
 
-install 'development tools' build-essential
+yum groupinstall "Development Tools"
 
-install Ruby ruby2.2 ruby2.2-dev
-update-alternatives --set ruby /usr/bin/ruby2.2 >/dev/null 2>&1
-update-alternatives --set gem /usr/bin/gem2.2 >/dev/null 2>&1
-
-echo installing Bundler
-gem install bundler -N >/dev/null 2>&1
+echo installing ruby, rails, bundler, etc
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable --ruby --rails
+source /usr/local/rvm/scripts/rvm
 
 install Git git
-install SQLite sqlite3 libsqlite3-dev
-install memcached memcached
-install Redis redis-server
-install RabbitMQ rabbitmq-server
+install SQLite sqlite sqlite-devel
+install Memcached memcached
 
-install PostgreSQL postgresql postgresql-contrib libpq-dev
-sudo -u postgres createuser --superuser vagrant
-sudo -u postgres createdb -O vagrant activerecord_unittest
-sudo -u postgres createdb -O vagrant activerecord_unittest2
+#uninstall MariaDB mariadb-server mariadb mariadb-libs mariadb-devel
+install MariaDB mariadb-server mariadb mariadb-libs mariadb-devel
+systemctl start mariadb
+#mysql_secure_installation
+systemctl enable mariadb.service
 
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-install MySQL mysql-server libmysqlclient-dev
-mysql -uroot -proot <<SQL
-CREATE USER 'rails'@'localhost';
-CREATE DATABASE activerecord_unittest  DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-CREATE DATABASE activerecord_unittest2 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON activerecord_unittest.* to 'rails'@'localhost';
-GRANT ALL PRIVILEGES ON activerecord_unittest2.* to 'rails'@'localhost';
-GRANT ALL PRIVILEGES ON inexistent_activerecord_unittest.* to 'rails'@'localhost';
+echo Creating rails user for mysql
+mysql -uroot <<SQL
+GRANT ALL PRIVILEGES ON *.* to 'rails'@'localhost';
+USE mysql;
+UPDATE user SET PASSWORD=PASSWORD("rails") WHERE User='rails';
+flush privileges;
+quit
 SQL
 
-install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
+install 'Nokogiri dependencies' libxml2 libxml2-devel libxslt1-devel
+install 'EPEL Repository' epel-release
 install 'ExecJS runtime' nodejs
+install Redis redis
+install RabbitMQ rabbitmq-server
 
 # Needed for docs generation.
-update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
+#update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
 
 echo 'all set, rock on!'
